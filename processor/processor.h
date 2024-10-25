7f76c48b3a859ac7ef6975fe8a182c3868cab1e3
h1gro <komarov.aa@phystech.edu>
2024-10-25 11:37:53 +0300

    processor on different directories

---
+++ processor/processor.h
@@ -0,0 +1,35 @@
+#ifndef PROCESSOR
+#define PROCESSOR
+
+const int NUM_REGISTERS = 5;
+
+struct spu_t
+{
+    struct stack_t stk;
+    int ip;
+    int* registers;
+    int* code;
+    int code_elems;
+};
+
+enum comands
+{
+    CMD_HLT   = 0,
+    CMD_PUSH  = 1,
+    CMD_SUB   = 2,
+    CMD_DIV   = 3,
+    CMD_ADD   = 4,
+    CMD_MUL   = 5,
+    CMD_OUT   = 6,
+    CMD_IN    = 7,
+    CMD_SQRT  = 8,
+    CMD_SIN   = 9,
+    CMD_COS   = 10,
+    CMD_DUMP  = 11,
+    CMD_JUMP  = 12,
+    CMD_JB    = 13,
+    CMD_JBE   = 14,
+    CMD_PUSHR = 15,
+};
+
+#endif
