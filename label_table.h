787ad1e99d779934dd2db94bf904ee781f9d2510
h1gro <komarov.aa@phystech.edu>
2024-10-25 11:43:36 +0300

    Delete label_table.h

--- label_table.h
+++
@@ -1,8 +0,0 @@
-#ifndef LABEL_TABLE
-#define LABEL_TABLE
-
-void LabelsCtor(struct label* labels);
-void LabelsDtor(struct label* labels);
-void FindMark(struct label* labels, struct file_t* file, int command, int ip_mark);
-
-#endif
